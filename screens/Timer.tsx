f049541707fe083bde9a2f2a05c3453ef2b89621
daniel <danielderu@gmail.com>
2022-09-07 08:27:37 +0200

    added timer to timer screen

--- screens/Timer.tsx
+++ screens/Timer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { View, Text, SafeAreaView, TouchableOpacity, StyleSheet } from "react-native"
 import globalStyle from '../globalStyles'
 import { NavigationScreenProp, NavigationState, NavigationParams } from "react-navigation"
@@ -15,18 +15,39 @@ interface Props {
 const Timer: React.FC<Props> = ({ navigation, route }): JSX.Element => {
     const [task, setTask] = useState<Task>(route.params)
     const [time, setTime] = useState<number>(task.duration)
+    const [timerActive, setTimerActive] = useState<boolean>(false)
 
-    const displayTime = () => {
-        const duration: DurationFormInterface = getDuration(time)
-        console.log(duration.days)
+    const tick = useRef<NodeJS.Timer>()
+    let buttonColor = timerActive ? colors.overdue : colors.active
+
+    const displayTime = (): string => {
+        const { floor } = Math
+        const days = time / (60 * 60 * 24)
+        const hours = (days % 1) * 24
+        const minutes = (hours % 1) * 60
+        const seconds = (minutes % 1) * 60
+
+        // Use get duration
+        
+
+        return `00:00:${pad(minutes)}:${pad(seconds)}`
+    }
+    const pad = (time: number): string => time < 10 ? `0${time}` : `${time}`
+
+    const timerCallback = (): void => {
+        if(timerActive){
+            tick.current = setInterval(() => setTime((prevTime: number) => --prevTime), 1000)
+        }
+        else {
+            clearInterval(tick.current)
+        }
     }
 
     useEffect(() => {
-        setInterval(() => {
-            console.log(time)
-            setTime((prevTime: number) => prevTime--)
-        }, 1000)
-    }, [])
+       timerCallback()
+
+       return () => clearInterval(tick.current)
+    }, [timerActive])
     return (
         <SafeAreaView style={[view.screenContainer, styles.container]}>
             <View>
@@ -34,11 +55,15 @@ const Timer: React.FC<Props> = ({ navigation, route }): JSX.Element => {
                 <Text style={styles.center}>Description</Text>
             </View>
             <View>
-                <Text style={[styles.center, text.heading]}>{time}</Text>
+                <Text style={[styles.center, text.heading]}>{displayTime()}</Text>
             </View>
             <View>
-                <TouchableOpacity style={buttons.fullWidth(colors.active)}>
-                    <Text style={text.button}>{"Start"} Timer</Text>
+                <TouchableOpacity
+                    activeOpacity={1}
+                    style={buttons.fullWidth(buttonColor)}
+                    onPress={() => setTimerActive(!timerActive)}
+                >
+                    <Text style={text.button}>{timerActive ? "Pause" : "Start"} Timer</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={buttons.fullWidth(colors.main)}>
                     <Text style={text.button}>Complete Task</Text>
